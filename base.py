e946a26e206bb05b5b5662621d2374e4e0d77061
dante <171379884@qq.com>
2021-09-29 10:45:47 +0800

    ==添加两个子类提供用户自定制headers和body==

--- base.py
+++ base.py
@@ -2,73 +2,72 @@
 import requests
 
 
+class RequestHeaders(object):
+    def __init__(self, content: dict):
+        self.content = content
+
+    def as_headers(self):
+        return self.content
+
+
+class RequestBody(object):
+    def __init__(self, body_info: dict):
+        self.body_info = body_info
+
+    def as_body(self):
+        return self.body_info
+
+
 class BaseRequestHandler(object):
-    def __init__(self, host):
+    def __init__(self, host, headers_info=None, body_info=None):
         self.host = host
-        # 默认请求头
-        self.headers = {'Content-Type': 'application/json'}
+        self.headers_info = headers_info
+        self.body_info = body_info
 
-    def _make_request_headers(self, request_url, request_method, request_body):
+    def _make_request_headers(self):
         """构造请求头"""
-        headers = {}
-        # 添加请求头信息
-        self.headers.update(headers)
+        if self.headers_info:
+            headers_object = RequestHeaders(self.headers_info)
+            headers = headers_object.as_headers()
+        else:
+            headers = {'Content-Type': 'application/json'}
+        return headers
 
-    @staticmethod
-    def _make_request_body(start_time, end_time, **kwargs):
-        request_body = {
-            'start_time': start_time,
-            'end_time': end_time,
-        }
-        if kwargs:
-            request_body.update(kwargs)
-        return request_body
+    def _make_request_body(self):
+        """构造请求体"""
+        if self.body_info:
+            body_object = RequestBody(self.body_info)
+            body = body_object.as_body()
+        else:
+            body = {}
+        return body
+
+    def _request(self, request_url, request_method='GET'):
+        """发送请求"""
+        url = self.host + request_url
+        request_body = self._make_request_body()
+        headers = self._make_request_headers()
+        retry_times = 0
+        while retry_times <= 3:
+            try:
+                if request_method == 'GET':
+                    response = getattr(requests, 'get')(url=url, params=request_body, headers=headers)
+                elif request_method == 'POST':
+                    response = getattr(requests, 'post')(url=url, json=request_body, headers=headers)
+                break
+            except Exception as e:
+                print(e)
+                retry_times += 1
+        return response
 
     @staticmethod
     def _handler_res(res):
         """处理返回结果"""
-        code = res.status_code
-        if code == 200:
-            return res.json()
-        elif code == 400:
-            print('do something')
-        else:
-            print('do something')
+        res_data = res.json()
+        return res_data
 
-    def _request(self, request_url, params, request_method='GET'):
-        """发送请求"""
-        url = self.host + request_url
-        request_body = self._make_request_body(**params)
-        self._make_request_headers(request_url, request_method='GET', request_body=request_body)
+    def request_get(self, url):
+        return self._request(url, request_method='GET')
 
-        try:
-            response = getattr(requests, request_method.lower())(url=url, json=request_body, headers=self.headers)
-        except Exception as e:
-            print(e)
-        return self._handler_res(response)
-
-    def request_get(self, url, params):
-        return self._request(url, params, request_method='GET')
-
-    def request_post(self, url, params):
-        return self._request(url, params, request_method='POST')
-
-    def get_top_stats_data(self, doc):
-        """获取TOP统计数据"""
-        url = '/reddripETC/top_stats'
-        return self.request_get(url, doc)
-
-    def get_summary_data(self, doc):
-        """获取概要统计信息"""
-        url = '/reddripETC/summary'
-        return self.request_get(url, doc)
-
-    def get_detect_trend_data(self, doc):
-        """获取检测结果趋势"""
-        url = '/reddripETC/detect_trend'
-        return self.request_get(url, doc)
-
-    def get_upload_trend_data(self, doc):
-        """获取邮件投递趋势"""
-        url = '/reddripETC/upload_trend'
-        return self.request_get(url, doc)
+    def request_post(self, url):
+        return self._request(url, request_method='POST')
